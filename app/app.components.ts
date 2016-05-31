1bd390288ca3fad4c8bd93cd15d70e6024386b28
Cory Franks and Garrett Biernat <garrett@>
2016-05-31 09:06:51 -0700

    initial commit

---
+++ app/app.components.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+
+export class AppComponent {
+  @Component({
+    selector: 'my-app',
+    template: `
+      <h1>Skeleton Angular2 App!</h1>
+    `
+  })
+}
